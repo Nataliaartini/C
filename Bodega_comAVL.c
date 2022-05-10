29da0ab8f49e11946542df109b9eba35426b57d6
Natalia Artini Ferrandin <79176834+Nataliaartini@users.noreply.github.com>
2022-05-10 11:12:25 -0300

    arrumando identação e arrumando comentarios em ingles

--- Bodega_comAVL.c
+++ Bodega_comAVL.c
@@ -7,127 +7,111 @@
 
 //structs
 
-struct bebidas
-{
+struct bebidas{
   int code;
   char nome [30];
   int volume;
   float preco;
   int estoque;
-  int tipo; //0 = não alcólica, 1 = alcólica   
+  int tipo; //0 = not alcooholic, 1 = alcooholic  
 };
 
 typedef struct bebidas Bebida;
 
-struct treenode
-{
+struct treenode{
   Bebida bebida;
   struct treenode *left, *right;
 };
 
 typedef struct treenode Node;
 
-struct tree
-{
+struct tree{
   Node *root;
 };
 
 typedef struct tree DrinkTree;
 
-//funções
+//functions
 
-void initTree(DrinkTree *t)
-{
+void initTree(DrinkTree *t){
   t->root = NULL;
 }
 
-void mostraMenu()
-{
+void mostraMenu(){
   printf("\n ?? BODEGAO DO JAO ?? \n");
-  printf("Escolha uma das opções abaixo:\n");
+  printf("Escolha uma das opï¿½ï¿½es abaixo:\n");
   printf("1 - Cadastrar bebida;\n2 - Listar bebidas;\n3 - Buscar bebida;\n4 - Excluir bebida;\n5 - Comprar bebida;\n6 - Vender bebida;\n7 - Sair\n?");
 }
 
-Node *searchCode(int code, Node *root)
-{
+Node *searchCode(int code, Node *root){
   if(root == NULL || code == root->bebida.code) 
     return root;
-  if(code < root->bebida.code) //se o código é menor procuramos na esquerda
+  if(code < root->bebida.code) //if the code is smaller, we search in the left
     return searchCode(code,root->left);
   return searchCode(code, root->right);
 }
 
-void printSingleNode(Node *n)
-{
-  printf("Código: %d\nNome: %s\nVolume: %d\nPreço: %.2f\nQuantidade em estoque: %d\nAlcoólica? ", n->bebida.code, n->bebida.nome, n->bebida.volume, 
+void printSingleNode(Node *n){
+  printf("Cï¿½digo: %d\nNome: %s\nVolume: %d\nPreï¿½o: %.2f\nQuantidade em estoque: %d\nAlcoï¿½lica? ", n->bebida.code, n->bebida.nome, n->bebida.volume, 
   n->bebida.preco, n->bebida.estoque);
 
   if(n->bebida.tipo == 1) printf("Sim\n\n");
-  else printf("Não\n\n");
+  else printf("Nï¿½o\n\n");
 }
 
-int validaDados(Bebida b , Node *root)
-{
+int validaDados(Bebida b , Node *root){
   int encontrouErro = 0;
   printf("\n");
 
-  if(searchCode(b.code, root))
-  {
-    printf("• Parça! se liga que você já tem esse código cadastrado!\n\n");
+  if(searchCode(b.code, root)){
+    printf("ï¿½ Parï¿½a! se liga que vocï¿½ jï¿½ tem esse cï¿½digo cadastrado!\n\n");
     printSingleNode(searchCode(b.code, root));  
     printf("\n");
     encontrouErro = 1;
   }
-  if(b.volume < 0)
-  {
-    printf("• Não vou nem comentar essa viagem! O volume deve ser positivo...\n");
+  if(b.volume < 0){
+    printf("ï¿½ Nï¿½o vou nem comentar essa viagem! O volume deve ser positivo...\n");
     encontrouErro = 1;
   }
-  if(b.preco < 0)
-  {
-    printf("• Oloco bicho, essa bebida é tão ruim que tu vai pagar pro cara levar? O preço não pode ser um valor negativo!\n");
+  if(b.preco < 0){
+    printf("ï¿½ Oloco bicho, essa bebida ï¿½ tï¿½o ruim que tu vai pagar pro cara levar? O preï¿½o nï¿½o pode ser um valor negativo!\n");
     encontrouErro = 1;
   }
-  if(b.estoque<0)
-  {
-    printf("• Ta querendo ganhar multa do leão é? O estoque não pode ficar negativo!\n");
+  if(b.estoque<0){
+    printf("ï¿½ Ta querendo ganhar multa do leï¿½o ï¿½? O estoque nï¿½o pode ficar negativo!\n");
     encontrouErro = 1;
   }
 
-  if(b.tipo != 1 && b.tipo != 0)
-  {
-    printf("• Não acha que está muito cedo pra já ter bebido? O tipo da bebida dever ser 1 ou 0!\n");
+  if(b.tipo != 1 && b.tipo != 0){
+    printf("ï¿½ Nï¿½o acha que estï¿½ muito cedo pra jï¿½ ter bebido? O tipo da bebida dever ser 1 ou 0!\n");
     encontrouErro = 1;
   }
-  if(encontrouErro)
-  {
-    printf("? O(s) dado(s) listado(s) acima estão inválido(s). Tente novamente! ? \n");
+  if(encontrouErro){
+    printf("? O(s) dado(s) listado(s) acima estï¿½o invï¿½lido(s). Tente novamente! ? \n");
     return encontrouErro;
   }
   else return encontrouErro;
 }
 
-Bebida recebeDados()
-{
+Bebida recebeDados(){
   Bebida b;
 
-  printf("\nCódigo:");
+  printf("\nCï¿½digo:");
   scanf("%d", &b.code); 
   printf("Nome:");
   scanf("%s", b.nome);
   printf("Volume:");
   scanf("%d", &b.volume);
-  printf("Preço:R$");
+  printf("Preï¿½o:R$");
   scanf("%f", &b.preco);
   printf("Quantidade de Estoque:");
   scanf("%d", &b.estoque);
-  printf("Digite 1 se a bebida é alcoólica e 0 se não:");
+  printf("Digite 1 se a bebida ï¿½ alcoï¿½lica e 0 se nï¿½o:");
   scanf("%d", &b.tipo);
   return b;
 }
 
-Node *newNode(Bebida b)
-{
+Node *newNode(Bebida b){
   Node *n;
   n=(Node *)malloc(sizeof(Node));
   n->bebida = b;
@@ -136,39 +120,32 @@ Node *newNode(Bebida b)
   return n;
 }
 
-void cadastrarBebida(DrinkTree *t)
-{
-  printf("\n ? CADASTRO DE BEBIDA ? \nInsira os dados listados abaixo, isso é uma ordem!\n");
+void cadastrarBebida(DrinkTree *t){
+  printf("\n ? CADASTRO DE BEBIDA ? \nInsira os dados listados abaixo, isso ï¿½ uma ordem!\n");
     
   Node *n, *aux, *previous;
   int dir;
   Bebida b = recebeDados();
-  while(validaDados(b, t->root))
-  {
+  while(validaDados(b, t->root)){
     b = recebeDados();
   }
   n = newNode(b);
-  if(t->root == NULL) //arvore vazia, salvamos a raiz
-  {
+  if(t->root == NULL){ //the tree is empty, we save the root
     t->root = n;
     return;
   }
-  else //encontrar o local na arvore para incluir
-  { 
-    aux=t->root; // começa na raiz
+  else{ //finding the place in the tree to include
+    aux=t->root; // comeï¿½a na raiz
 	  dir=0;
-    while (aux!=NULL)
-    {
+    while (aux!=NULL){
       previous=aux;
-      if (n->bebida.code < aux->bebida.code) // novo código é menor, vai para a esquerda
-      {
+      if (n->bebida.code < aux->bebida.code){ // new code is smaller, goes to left
         aux=aux->left;
-        dir=0; // o novo nodo deve ser inserido como filho da esquerda
+        dir=0; // new node must be inserted as a son of left
       }
-      else
-      {
-          aux=aux->right;  // se não, é maior vai a direita
-          dir=1; // o novo nodo deve ser inserido como filho da direita
+      else{
+          aux=aux->right;  // else, the bigger goes to right
+          dir=1; // new node must be inserted as a son of right
       }
     }
       if (dir) previous->right=n;
@@ -177,8 +154,7 @@ void cadastrarBebida(DrinkTree *t)
   }
 }
 
-void imprimeTree(Node *root)
-{
+void imprimeTree(Node *root){
   if (root==NULL) 
 	{
 		printf("\n");
@@ -189,25 +165,23 @@ void imprimeTree(Node *root)
 	imprimeTree(root->right); 
 }
 
-void buscaBebida(Node *root)
-{
+void buscaBebida(Node *root){
   int code;
   Node *aux;
   printf("\n ? PROCURAR BEBIDA ? \n\n");
-  printf("Digite o código da bebida que deseja achar, você tem 3 desejos! Just kidding.\n?");
+  printf("Digite o cï¿½digo da bebida que deseja achar, vocï¿½ tem 3 desejos! Just kidding.\n?");
   scanf("%d", &code);
   aux = searchCode(code, root);
   if(aux == NULL) 
-  printf("Não achei essa bebida nos seu estoque! oops \n"); 
+  printf("Nï¿½o achei essa bebida nos seu estoque! oops \n"); 
   else
   {
-    printf("\n ? Aqui está, chefe! ? \n\n");
+    printf("\n ? Aqui estï¿½, chefe! ? \n\n");
     printSingleNode(aux);
   }
 }
 
-Node *maxChild(Node *root)
-{
+Node *maxChild(Node *root){
   Node *promoted;
   if (root->right->right == NULL)
   {
@@ -219,11 +193,10 @@ Node *maxChild(Node *root)
   return maxChild(root->right);
 }
 
-Node *deleteNode(Node *root, int code)
-{
+Node *deleteNode(Node *root, int code){
   Node *newRoot;
   if (root == NULL){
-    printf("Não achei essa bebida nos seu estoque! oops!\n");
+    printf("Nï¿½o achei essa bebida nos seu estoque! oops!\n");
     return root;
   }
   if (root->bebida.code == code) { //Se for folha;
@@ -232,7 +205,7 @@ Node *deleteNode(Node *root, int code)
       return NULL;
     } //SE TEM UM FILHO
       if (root->left == NULL || root->right == NULL) {
-        newRoot = (root->left == NULL) ? root->right : root->left; //if ternário
+        newRoot = (root->left == NULL) ? root->right : root->left; //if ternï¿½rio
         free(root);
         return newRoot;
       } 
@@ -258,32 +231,29 @@ Node *deleteNode(Node *root, int code)
   return root;
 }
 
-void excluirBebida(DrinkTree *t)
-{
+void excluirBebida(DrinkTree *t){
   int code;
   Node *aux;
   printf("\n ? EXCLUIR BEBIDA ? \n\n");
-  printf("TEM CERTEZA QUE DESEJA FAZER ISSO? NÃO TERÁ VOLTA AMIGÃO!!! qual o código da bebida que queres excluir?\n?");
+  printf("TEM CERTEZA QUE DESEJA FAZER ISSO? Nï¿½O TERï¿½ VOLTA AMIGï¿½O!!! qual o cï¿½digo da bebida que queres excluir?\n?");
   scanf("%d", &code);
   t->root = deleteNode(t->root, code);
 }
 
-void comprarBebida(Node *root)
-{
+void comprarBebida(Node *root){
   int code;
   Node *aux;
   printf("\n ? COMPRAR BEBIDA ? \n\n");
-  printf("Diz aí o código da Bebidinha que tu comprou!\n?");
+  printf("Diz aï¿½ o cï¿½digo da Bebidinha que tu comprou!\n?");
   scanf("%d", &code);
   aux = searchCode(code, root);
   if(aux == NULL) 
-  printf("Rapaz, essa bebida não está cadastrada, volte uma casa! \n");
+  printf("Rapaz, essa bebida nï¿½o estï¿½ cadastrada, volte uma casa! \n");
   
-  else
-  {
+  else{
     int qtd;
     printf("\n ? Cadastro Encontrado ? \n\n");
-    printf("Quantas unidades desse licor dos deuses você comprou?\n?");
+    printf("Quantas unidades desse licor dos deuses vocï¿½ comprou?\n?");
     scanf("%d", &qtd);
     aux->bebida.estoque = aux->bebida.estoque + qtd;
     printf("\n ? Compra realizada! Cadastro Atualizado! ?\n\n");
@@ -291,17 +261,16 @@ void comprarBebida(Node *root)
   }
 }
 
-void venderBebida(Node *root)
-{
+void venderBebida(Node *root){
   int code, idade = 18, qtd, mes, dia, ano;
   Node *aux;
   printf("\n ? VENDER BEBIDA ? \n\n");
-  printf("AEEE conseguiu fechar uma venda! Qual o código do licor dos deuses você finalmente vendeu?\n?");
+  printf("AEEE conseguiu fechar uma venda! Qual o cï¿½digo do licor dos deuses vocï¿½ finalmente vendeu?\n?");
   scanf("%d", &code);
   aux = searchCode(code, root);
   if(aux == NULL)
   {
-    printf("Rapaz, essa bebida não está cadastrada, volte uma casa! \n");
+    printf("Rapaz, essa bebida nï¿½o estï¿½ cadastrada, volte uma casa! \n");
     return;
   }
 
@@ -313,24 +282,19 @@ void venderBebida(Node *root)
   time(&segundos); 
   data_atual = localtime(&segundos);
   
-  if(aux->bebida.tipo == 1)
-  {
-    if ((data_atual->tm_year+1900 - ano) < 18)
-    {
-      printf("\n?Proibido vender bebidas alcoólicas para menores de 18 anos seu mané!?\n\n");
+  if(aux->bebida.tipo == 1){
+    if ((data_atual->tm_year+1900 - ano) < 18){
+      printf("\n?Proibido vender bebidas alcoï¿½licas para menores de 18 anos seu manï¿½!?\n\n");
       return;
     }
     
-    if (data_atual->tm_year+1900 - ano == 18)
-    {
-      if(data_atual->tm_mon+1 - mes < 0)
-      {
-        printf("\n?Proibido vender bebidas alcoólicas para menores de 18 anos seu mané!?\n\n");
+    if (data_atual->tm_year+1900 - ano == 18){
+      if(data_atual->tm_mon+1 - mes < 0){
+        printf("\n?Proibido vender bebidas alcoï¿½licas para menores de 18 anos seu manï¿½!?\n\n");
         return;
       }
-      if(data_atual->tm_mday-1 - dia < 0)
-      {
-        printf("\n?Proibido vender bebidas alcoólicas para menores de 18 anos seu mané!?\n\n");
+      if(data_atual->tm_mday-1 - dia < 0){
+        printf("\n?Proibido vender bebidas alcoï¿½licas para menores de 18 anos seu manï¿½!?\n\n");
         return;
       }
     }
@@ -338,17 +302,15 @@ void venderBebida(Node *root)
 
   printf("Quantidade de venda\n?");
   scanf("%d", &qtd);
-  if(qtd > aux->bebida.estoque)
-  {
-    printf("\n ? Sem estoque suficiente, irmão! ?\n\n");
+  if(qtd > aux->bebida.estoque){
+    printf("\n ? Sem estoque suficiente, irmï¿½o! ?\n\n");
     return;
   }
   aux->bebida.estoque = aux->bebida.estoque - qtd;  
   printf("\n ? AEEE conseguiu fechar uma venda! ? \n\n");
 }
 
-void freeMemory(Node *root)
-{
+void freeMemory(Node *root){
   if (root==NULL) return;
 	freeMemory(root->left);
 	freeMemory(root->right);
@@ -357,20 +319,17 @@ void freeMemory(Node *root)
 
 //main
 
-int main ()
-{
+int main (){
   setlocale(LC_ALL, "Portuguese");
   DrinkTree t;
   initTree(&t);
   int opcao = 0;
 
-  while(opcao != 7)
-  { 
+  while(opcao != 7){ 
     mostraMenu();
     scanf("%d", &opcao);
       
-    switch(opcao)
-    {
+    switch(opcao){
       case 1:
         cadastrarBebida(&t);
         break;   
@@ -394,10 +353,9 @@ int main ()
         freeMemory(t.root);
         break;
       default:
-        printf(" ? Nem tem essa opção seu maluco! Vacilou! Vou te dar mais uma chance, vai: ?\n");
+        printf(" ? Nem tem essa opï¿½ï¿½o seu maluco! Vacilou! Vou te dar mais uma chance, vai: ?\n");
         break;
      }
     }
   return 0;
 }
-
